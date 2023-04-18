fce35ccb1b18d1b0f3cf656723d639295ac6198f
BeeMOre32 <windowssart01@gmail.com>
2023-04-18 20:17:36 +0900

    ⚙️conf: 기본적인 스타일 변수선언

---
+++ src/styled.d.ts
@@ -0,0 +1,16 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    background: {
+      main: string;
+      light: string;
+      dark: string;
+    };
+    text: string;
+    loading: string;
+    primary: string;
+    warning: string;
+    danger: string;
+  }
+}
---
+++ src/theme.ts
@@ -0,0 +1,14 @@
+import { DefaultTheme } from 'styled-components';
+
+export const theme: DefaultTheme = {
+  background: {
+    main: '#212437',
+    light: '#292c43',
+    dark: '#171721',
+  },
+  text: '#f5f5fa',
+  loading: '#e4e6e8',
+  primary: '#19c37d',
+  warning: '#f7b500',
+  danger: '#ef4146',
+};
