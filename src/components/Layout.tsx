2a6f45734c5ec2221a78e34e75e5da650f7ee619
BeeMOre32 <windowssart01@gmail.com>
2023-04-21 15:35:09 +0900

    🏭refact: header 컴포넌트를 레이아웃화

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,26 +2,27 @@ import React from 'react';
 import * as S from '@/styles/styled';
 import Logo from '@/components/Logo';
 import Link from 'next/link';
-import { MainContentDummy } from '@/styles/styled';
 
-function Layout({ children }: { children: React.ReactNode }) {
+function Layout({ children, hasHeader }: { children: React.ReactNode; hasHeader: boolean }) {
   return (
     <S.MainWrapper>
-      <S.Header>
-        <Link href={'/'}>
-          <div className="logo">
-            <Logo />
-          </div>
-        </Link>
-        <div>
-          <S.HeaderWriteButton whileHover={{ scale: 1.03 }} whileTap={{ scale: 0.97 }}>
-            글쓰기
-          </S.HeaderWriteButton>
-          <Link href="/profile">
-            <S.HeaderProfile></S.HeaderProfile>
+      {hasHeader && (
+        <S.Header>
+          <Link href={'/'}>
+            <div className="logo">
+              <Logo />
+            </div>
           </Link>
-        </div>
-      </S.Header>
+          <div>
+            <S.HeaderWriteButton whileHover={{ scale: 1.03 }} whileTap={{ scale: 0.97 }}>
+              글쓰기
+            </S.HeaderWriteButton>
+            <Link href="/profile">
+              <S.HeaderProfile></S.HeaderProfile>
+            </Link>
+          </div>
+        </S.Header>
+      )}
       {children}
     </S.MainWrapper>
   );
