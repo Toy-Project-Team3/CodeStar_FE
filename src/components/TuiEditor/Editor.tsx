bb0d327b27112232c60be194b4acf4d350c6b489
youngeun <jessicak0828@gmail.com>
2023-04-25 23:28:42 +0900

    📜feat: Editor 컴포넌트 부가 기능 구현 viewport width 값에 따른 editor 모드 변환, editor 리셋버튼 추가

--- src/components/TuiEditor/Editor.tsx
+++ src/components/TuiEditor/Editor.tsx
@@ -1,68 +1,66 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Editor } from '@toast-ui/react-editor';
 import '@toast-ui/editor/dist/toastui-editor.css';
 import '@toast-ui/editor/dist/theme/toastui-editor-dark.css';
 import '@toast-ui/editor/dist/i18n/ko-kr';
 
 export default function ToastEditor(props) {
+  const oldContents = props.contents;
   const getEditorContents = props.getEditorContents;
   const viewWidth = props.viewWidth;
-  const editorRef = useRef(null);
+  const editorRef = useRef({});
+
+  useEffect(() => {
+    editorRef.current.getInstance().insertText(oldContents);
+  }, []);
+  useEffect(() => {
+    if (viewWidth < 720) {
+      editorRef.current.getInstance().changeMode('wysiwyg');
+    } else {
+      editorRef.current.getInstance().changeMode('markdown');
+    }
+  }, [viewWidth]);
+
+  // useEffect(() => {
+  //   const EditorElements = editorRef.current.getInstance().getEditorElements();
+  //   console.log(EditorElements.mdEditor.innerHTML);
+  //   console.log(EditorElements.mdPreview.innerHTML);
+  //   console.log(EditorElements.wwEditor.innerHTML);
+  // }, []);
   const liftEditorContents = function () {
-    //console.log(editorRef.current.getInstance());
     console.log(editorRef.current.getInstance().getMarkdown());
     console.log(editorRef.current.getInstance().getHTML());
     getEditorContents(editorRef.current.getInstance().getMarkdown());
-    //setContents(editorRef.current.getInstance().getMarkdown())
+  };
+
+  const resetHandler = () => {
+    editorRef.current.getInstance().reset();
   };
 
   return (
     <div>
-      {viewWidth >= 720 ? (
-        <Editor
-          ref={editorRef}
-          onChange={liftEditorContents}
-          previewStyle="vertical" // 미리보기 스타일 지정
-          previewHighlight={false}
-          language="ko-KR"
-          initialEditType="markdown"
-          hideModeSwitch={false}
-          initialValue=" "
-          placeholder="당신의 이야기를 적어보세요..."
-          useCommandShortcut={true}
-          toolbarItems={[
-            ['heading', 'bold', 'italic', 'strike'],
-            ['hr', 'quote'],
-            ['task'],
-            ['table', 'image', 'link'],
-            ['codeblock'],
-          ]}
-          height="75vh"
-          theme="dark"
-        />
-      ) : (
-        <Editor
-          ref={editorRef}
-          onChange={liftEditorContents}
-          previewStyle="vertical" // 미리보기 스타일 지정
-          previewHighlight={false}
-          language="ko-KR"
-          initialEditType="wysiwyg"
-          hideModeSwitch={true}
-          initialValue=" "
-          placeholder="당신의 이야기를 적어보세요..."
-          useCommandShortcut={true}
-          toolbarItems={[
-            ['heading', 'bold', 'italic', 'strike'],
-            ['hr', 'quote'],
-            ['task'],
-            ['table', 'image', 'link'],
-            ['codeblock'],
-          ]}
-          height="75vh"
-          theme="dark"
-        />
-      )}
+      <button onClick={resetHandler}>reset</button>
+      <Editor
+        ref={editorRef}
+        onChange={liftEditorContents}
+        previewStyle="vertical" // 미리보기 스타일 지정
+        previewHighlight={false}
+        language="ko-KR"
+        initialEditType={'markdown'}
+        hideModeSwitch={true}
+        initialValue=" "
+        placeholder="당신의 이야기를 적어보세요..."
+        useCommandShortcut={true}
+        toolbarItems={[
+          ['heading', 'bold', 'italic', 'strike'],
+          ['hr', 'quote'],
+          ['task'],
+          ['table', 'image', 'link'],
+          ['code', 'codeblock'],
+        ]}
+        height="75vh"
+        theme="dark"
+      />
     </div>
   );
 }
