72c8d9662cd967693ba3152143b02d5904da6a40
BeeMOre32 <windowssart01@gmail.com>
2023-04-25 21:01:07 +0900

    📜feat: 프로필 페이지 레이아웃 구현 완료

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
-import * as S from '@/styles/profileStyled';
-import { ProfileActivity } from '@/styles/profileStyled';
-import { motion, Variants } from 'framer-motion';
-import CreditScore from '@/components/CreditScore';
-import BaseLayout from '@/components/Layout/BaseLayout';
 import ProfileLayout from '@/components/Layout/ProfileLayout';
 
 function Index() {
   return (
     <ProfileLayout hasHeader={true}>
       <h1 className="none-content">
-        프로필의 메인 페이지 입니다.
-        <br /> 위의 표 밑에 있는 숫자를 클릭하여 활동 내역을 확인 할 수 있습니다.
+        프로필의 메인 페이지 입니다. 위의 표 밑에 있는 숫자를 클릭하여 활동 내역을 확인 할 수 있습니다.
       </h1>
     </ProfileLayout>
   );
