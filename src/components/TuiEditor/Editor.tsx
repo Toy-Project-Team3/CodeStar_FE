0c55b97637d9abf13345cb4ab532dcfbc072154f
youngeun <jessicak0828@gmail.com>
2023-04-26 17:19:42 +0900

    📜feat: Toast UI Editor 반응형 수정

--- src/components/TuiEditor/Editor.tsx
+++ src/components/TuiEditor/Editor.tsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, forwardRef, useImperativeHandle } from 'react';
 import { Editor } from '@toast-ui/react-editor';
 import '@toast-ui/editor/dist/toastui-editor.css';
 import '@toast-ui/editor/dist/theme/toastui-editor-dark.css';
 import '@toast-ui/editor/dist/i18n/ko-kr';
 
-export default function ToastEditor(props) {
+const ToastEditor = forwardRef((props, toastEditorRef) => {
   const oldContents = props.contents;
   const getEditorContents = props.getEditorContents;
   const viewWidth = props.viewWidth;
@@ -14,7 +14,7 @@ export default function ToastEditor(props) {
     editorRef.current.getInstance().insertText(oldContents);
   }, []);
   useEffect(() => {
-    if (viewWidth < 720) {
+    if (viewWidth < 800) {
       editorRef.current.getInstance().changeMode('wysiwyg');
     } else {
       editorRef.current.getInstance().changeMode('markdown');
@@ -32,35 +32,35 @@ export default function ToastEditor(props) {
     console.log(editorRef.current.getInstance().getHTML());
     getEditorContents(editorRef.current.getInstance().getMarkdown());
   };
-
-  const resetHandler = () => {
+  function resetEditor() {
     editorRef.current.getInstance().reset();
-  };
+  }
+  useImperativeHandle(toastEditorRef, () => ({
+    resetEditor,
+  }));
 
   return (
-    <div>
-      <button onClick={resetHandler}>reset</button>
-      <Editor
-        ref={editorRef}
-        onChange={liftEditorContents}
-        previewStyle="vertical" // 미리보기 스타일 지정
-        previewHighlight={false}
-        language="ko-KR"
-        initialEditType={'markdown'}
-        hideModeSwitch={true}
-        initialValue=" "
-        placeholder="당신의 이야기를 적어보세요..."
-        useCommandShortcut={true}
-        toolbarItems={[
-          ['heading', 'bold', 'italic', 'strike'],
-          ['hr', 'quote'],
-          ['task'],
-          ['table', 'image', 'link'],
-          ['code', 'codeblock'],
-        ]}
-        height="75vh"
-        theme="dark"
-      />
-    </div>
+    <Editor
+      ref={editorRef}
+      onChange={liftEditorContents}
+      previewStyle="vertical" // 미리보기 스타일 지정
+      previewHighlight={false}
+      language="ko-KR"
+      initialEditType={'markdown'}
+      hideModeSwitch={true}
+      initialValue=" "
+      placeholder="당신의 이야기를 적어보세요..."
+      useCommandShortcut={true}
+      toolbarItems={[
+        ['heading', 'bold', 'italic', 'strike'],
+        ['hr', 'quote'],
+        ['task'],
+        ['table', 'image', 'link'],
+        ['code', 'codeblock'],
+      ]}
+      height="75vh"
+      theme="dark"
+    />
   );
-}
+});
+export default ToastEditor;
