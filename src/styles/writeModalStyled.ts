60e2a1273a106b01d36a12047267aa92d89f8eee
youngeun <jessicak0828@gmail.com>
2023-04-26 17:19:23 +0900

    📜feat: 글쓰기 페이지 확인 모달 (/posts) 레이아웃 완성

--- src/styles/writeModalStyled.ts
+++ src/styles/writeModalStyled.ts
@@ -2,38 +2,46 @@ import styled from 'styled-components';
 import { Button } from '@/styles/buttonStyled';
 
 export const Background = styled.div`
+  position: absolute;
+  z-index: 9;
   padding: 20vh 10vw;
   box-sizing: border-box;
   width: 100vw;
   height: 100vh;
-  background-color: pink;
+  background-color: ${(p) => p.theme.color.dark};
   display: flex;
   justify-content: center;
   align-items: center;
-
-  /* &:focus {
-    transform: translateY(-100vh);
-  } */
+  transform: translate(100px);
 `;
 export const Container = styled.div`
   padding: 3.2rem;
-  height: 42.5rem;
+  height: 50rem;
   width: 35.1rem;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  gap: 1rem;
+  .btnContainer {
+    padding: 0 0 0.5rem 0;
+    display: flex;
+    justify-content: flex-end;
+  }
   h3 {
     font-size: 1.6rem;
-    margin-bottom: 0.8rem;
+    margin: 1rem 0 0.5rem 0.5rem;
+    color: ${(p) => p.theme.color.light};
   }
   h4 {
-    font-size: 3.5rem;
+    height: 3.5rem;
+    padding: 0.7rem 1rem;
+    font-size: 2.5rem;
+    background-color: ${(p) => p.theme.color.dark2};
+    color: #fff;
   }
-  .thumbnailContainer {
+  .thumbnailImgContainer {
+    background-color: ${(p) => p.theme.color.dark2};
     height: 19.3rem;
     position: relative;
-    border: 0.1rem solid black;
   }
   .emptyThumbnail {
     width: 100%;
@@ -42,25 +50,31 @@ export const Container = styled.div`
     flex-direction: column;
     justify-content: space-evenly;
     align-items: center;
+    margin-bottom: 1rem;
   }
   label {
-    border: 1px solid black;
+    background-color: #202020;
+    color: ${(p) => p.theme.color.darkHighlight};
     border-radius: 0.4rem;
     height: 4rem;
     padding: 0 2rem;
+    font-family: 'Pretendard Regular';
     font-weight: 800;
     font-size: 1.8rem;
     cursor: pointer;
     display: flex;
     align-items: center;
+    &:hover {
+      opacity: 0.8;
+    }
   }
   .buttons {
     display: flex;
     justify-content: flex-end;
     gap: 1rem;
-    button {
-      outline: none;
-      background: none;
+    .active {
+      border: 1px solid ${(p) => p.theme.color.highlight};
+      color: ${(p) => p.theme.color.highlight};
     }
   }
 `;
