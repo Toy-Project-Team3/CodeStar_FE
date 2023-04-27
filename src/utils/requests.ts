a559ba898f7d0b48e105e1b27b37593c253f55c3
seon-mikim <tjsalsun@gmail.com>
2023-04-28 02:24:51 +0900

    feat: 함수 생성

--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -1,5 +1,10 @@
 import { instance } from '@/utils/axiosInstance';
 import { PostInterface } from '@/types/RequestInterface';
+import { User } from '@/types/User';
+
+
+
+
 
 export const getPosts = async () => {
   const res = await instance.get<PostInterface[]>(`posts`);
@@ -16,10 +21,21 @@ export const getblogPosts = async (userId: string) => {
   return res.data;
 };
 
-export const createPost = async (post) => {
+export const createPost = async (post: any) => {
   const response = await instance.post('posts', post);
   return response.data;
 };
+
+export const updatePost = async (postId: string) => {
+  const response = await instance.put(`posts/${postId}`)
+  return response.data
+}
+
+export const deletePost = async (userId: string, postId: string)=> {
+ const response = await instance.delete(`posts/${userId}/${postId}`)
+ return response.data
+}
+
 export const postLikeDislike = async (userId: String, postId: String) => {
   const response = await instance.post(`posts/${userId}/${postId}/like`);
   return response.data;
@@ -29,3 +45,14 @@ export const postComment = async (userId: String, postId: String, post: string) 
   const response = await instance.post(`posts/${userId}/${postId}/comments`, post);
   return response.data;
 };
+
+
+export const getMyInfo = async (userId: string): Promise<User>=> {
+ const response = await instance.get(`auth/mypage/${userId}`)
+ return response.data
+}
+
+export const updateMyInfo = async (userId: string): Promise<User>=> {
+ const response = await instance.get(`auth/mypage/${userId}`)
+ return response.data
+}
